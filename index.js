24666d4ce733cae82302d0ae14ce3d8a84fba897
Grace Tian <gracetian4ever@gmail.com>
2020-03-11 20:47:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ function takeANumber(array,name){
   return `Welcome, ${name}. You are number ${array.length} in line.`
 }
 function nowServing(array){
-  if(array.length){
-    return `Currently serving ${array.shift()}.`
-  }
-  return "There is nobody waiting to be served!"
+  return array.length? `Currently serving ${array.shift()}.` : "There is nobody waiting to be served!"
+  // if(array.length){
+  //   return `Currently serving ${array.shift()}.`
+  // }
+  // return "There is nobody waiting to be served!"
 }
 function currentLine(array){
   let str=array.map((x,index)=>` ${index+1}. ${x}`).join(",")
